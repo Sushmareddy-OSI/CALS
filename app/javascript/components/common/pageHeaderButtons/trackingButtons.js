895c1fd59f04fe4cb389d2864b88340c9635e56d
Keith Lauher <klauher@cambriasolutions.com>
2018-06-18 13:58:55 -0700

    lint

--- app/javascript/components/common/pageHeaderButtons/trackingButtons.js
+++ app/javascript/components/common/pageHeaderButtons/trackingButtons.js
@@ -11,36 +11,35 @@ const TrackingButtons = ({
   return (
     <div>
       {
-      editMode ?
-        <div className='pull-right' >
-          <div >
-            <div className='tracking-cancel inlineBlock'>
-              <Button
-                buttonId='CancelTracking'
-                label='Cancel'
-                textAlignment='center'
-                onClick={cancelProgress} />
-            </div>
-            <div className='tracking-button inlineBlock'>
-              <Button
-                buttonId='saveTracking'
-                label='Save'
-                textAlignment='center'
-                onClick={saveProgress} />
+        editMode
+          ? <div className='pull-right' >
+            <div >
+              <div className='tracking-cancel inlineBlock'>
+                <Button
+                  buttonId='CancelTracking'
+                  label='Cancel'
+                  textAlignment='center'
+                  onClick={cancelProgress} />
+              </div>
+              <div className='tracking-button inlineBlock'>
+                <Button
+                  buttonId='saveTracking'
+                  label='Save'
+                  textAlignment='center'
+                  onClick={saveProgress} />
+              </div>
             </div>
           </div>
-        </div>
-        :
-        <div className='pull-right'>
-          <div className='tracking-button'>
-            <Button
-              buttonId='editTracking'
-              label='Edit Checklist'
-              textAlignment='center'
-              onClick={editProgress} />
+          : <div className='pull-right'>
+            <div className='tracking-button'>
+              <Button
+                buttonId='editTracking'
+                label='Edit Checklist'
+                textAlignment='center'
+                onClick={editProgress} />
+            </div>
           </div>
-        </div>
-  }
+      }
     </div>
   )
 }
